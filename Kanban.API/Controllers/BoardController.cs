f1111a795679e806ba28739dde7c47e741007c2e
Lance <lance100@iastate.edu>
2024-07-30 15:40:40 -0400

    Added more styling and ability to edit basic card info

--- Kanban.API/Controllers/BoardController.cs
+++ Kanban.API/Controllers/BoardController.cs
@@ -124,4 +124,27 @@ public class BoardController : Controller
 
         return StatusCode (StatusCodes.Status418ImATeapot);
     }
+
+    [HttpDelete ("delete/{ID:guid}")] // Need to delete from board AND card tables. There might also be extensions to remove. For now though, I'm just going to do board.
+    public async Task<ActionResult> DeleteCard (Guid ID)
+    {
+        var boardList = new List<Board> ();
+        var boardsFromTable = _boardTable.QueryAsync<Board> (board => board.RowKey == ID.ToString ());
+        await foreach (var board in boardsFromTable)
+            boardList.Add (board);
+
+        if (boardList.Count () is 0)
+            return NotFound ("The board card you are searching for was not found.");
+
+        if (boardList.Count () > 1)
+            return StatusCode (StatusCodes.Status500InternalServerError, "Multiple board cards were found with the same ID.");
+
+        var boardFromDatabase = boardList.Single ();
+        var cardToDelete = await _boardTable.DeleteEntityAsync (boardFromDatabase.PartitionKey, boardFromDatabase.RowKey);
+
+        if (!cardToDelete.IsError)
+            return Ok (); //Is there a better Status to return? NoContent perhaps?
+        else
+            return StatusCode (StatusCodes.Status500InternalServerError, "Could not delete board card.");
+    }
 }
