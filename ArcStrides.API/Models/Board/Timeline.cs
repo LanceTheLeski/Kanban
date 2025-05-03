bc38fd0ad6e3d8b98728b44b0dad125f7fd3a3c7
Lance <lance100@iastate.edu>
2025-05-03 12:39:07 -0500

    Started editing timelines more

--- ArcStrides.API/Models/Board/Timeline.cs
+++ ArcStrides.API/Models/Board/Timeline.cs
@@ -10,9 +10,8 @@ public class Timeline : ArcBoardsEntity
     /// </summary>
     public override string RowKey { get; set; }
 
-    public Guid ParentObjectID { get; set; } //Parent Object ID (Card/Task)
+    public Guid ParentObjectID { get; set; }
 
-    //Ideally this only refers to what type of parent the Timeline has: Card or Task? Could also be used to identify what type of deadlines and all are set.
     public int TimelineTypeID { get; set; }
 
     // Everything that needs to be done prior to start. Should probably link to a TAG GROUP GUID with children being Tasks. This deadline would obviously be the parent of the group.
---
+++ ArcStrides.API/Models/TagGroup/TimelineType.cs
@@ -0,0 +1,16 @@
+﻿using ArcStrides.API.Attributes;
+
+namespace ArcStrides.API.Models.TagGroup;
+
+[ArcTableName ("TimelineTypes")]
+public class TimelineType : ArcTagGroupsEntity
+{
+    /// <summary>
+    /// ID of the timeline type as an integer.
+    /// Indicates deadline or (proper) timeline.
+    /// Also indicates Card or Task parent.
+    /// </summary>
+    public string RowKey { get; set; }
+
+    public string Title { get; set; }
+}
